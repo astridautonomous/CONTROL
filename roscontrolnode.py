6253d7f4c55f1e0cc0f0a40e240f397faecbc790
egeparpar <157587173+egeparpar@users.noreply.github.com>
2024-03-28 06:40:35 +0300

    control node completed

--- roscontrolnode.py
+++ roscontrolnode.py
@@ -1,4 +1,4 @@
-#!/home/ege/anaconda3/envs/deneme/bin python3
+#!/home/ege/anaconda3/envs/rosbridge-carla/bin python3
 
 import rospy
 import numpy as np
@@ -8,8 +8,17 @@ from nav_msgs.msg import Odometry
 from traj import TrajectoryProcess
 import numpy as np
 
-# Create numpy array from [0,0] to [100,100] with step size 1
-refposedeneme = np.array(np.meshgrid(np.arange(101), np.arange(101))).T.reshape(-1, 2)
+#example trajectory
+start_point = np.array([-74, -143])
+end_point = np.array([-80, -70])
+
+refposedeneme = np.linspace(start_point, end_point, 300)
+current_velocity=0
+
+def velocity_callback(data):
+    global current_velocity
+    current_velocity = data.velocity
+
 
 def pose_callback(data):
     global current_pose
@@ -21,16 +30,16 @@ def pose_callback(data):
         data.pose.pose.orientation.z])
     current_pose = np.array([data.pose.pose.position.x, data.pose.pose.position.y])
     msg = CarlaEgoVehicleControl()
-    steer = TrajectoryProcess(refposedeneme).process_poses_stanley(current_pose, yaw, current_velocity)
+    steercmd = TrajectoryProcess(refposedeneme).process_poses_stanley(current_pose, yaw, current_velocity)
 
-    msg.steer = steer
-    msg.throttle = 1
+    v_error = 10 - current_velocity
+    brakecmd=TrajectoryProcess(refposedeneme).pidbrake(v_error,0.1)
+    throttlecmd = TrajectoryProcess(refposedeneme).pidthrottle(v_error,0.1)
+    
+    msg.brake = brakecmd
+    msg.throttle = throttlecmd
+    msg.steer = steercmd
     pub.publish(msg)
-
-def velocity_callback(data):
-    global current_velocity
-    current_velocity = data.velocity
-
 
 
 if __name__ == "__main__":
@@ -38,6 +47,6 @@ if __name__ == "__main__":
     sub1 = rospy.Subscriber("/carla/ego_vehicle/odometry", Odometry, callback=pose_callback)
     sub2 = rospy.Subscriber("/carla/ego_vehicle/vehicle_status", CarlaEgoVehicleStatus, callback=velocity_callback)
     rospy.loginfo("Node has been started!")
-    pub = rospy.Publisher("/carla/ego_vehicle/", CarlaEgoVehicleControl)
+    pub = rospy.Publisher("/carla/ego_vehicle/vehicle_control_cmd", CarlaEgoVehicleControl, queue_size=10)
 
     rospy.spin()
